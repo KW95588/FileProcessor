d6391b2e9caf37c781fb5496aa5ddeb7b65e3596
K W <kw95588@outlook.com>
2019-01-24 14:02:41 +1300

    Add code

--- FileProcessor/FileProcessor/Form1.Designer.cs
+++ FileProcessor/FileProcessor/Form1.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.InvoiceNumber = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(133, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Data File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(127, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Invoice Number";
+            // 
+            // InvoiceNumber
+            // 
+            this.InvoiceNumber.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InvoiceNumber.Location = new System.Drawing.Point(318, 249);
+            this.InvoiceNumber.Name = "InvoiceNumber";
+            this.InvoiceNumber.Size = new System.Drawing.Size(196, 38);
+            this.InvoiceNumber.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(551, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Extract File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(954, 570);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.InvoiceNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox InvoiceNumber;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- FileProcessor/FileProcessor/Form1.cs
+++ FileProcessor/FileProcessor/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,154 @@ namespace FileProcessor
 {
     public partial class Form1 : Form
     {
+        string fileName;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Open File";
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                fileName = openFileDialog.FileName;
+                MessageBox.Show(fileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string inputInvoiceNumber = InvoiceNumber.Text.Trim();
+            MessageBox.Show("Invoice number: " + inputInvoiceNumber);
+            //MessageBox.Show(fileName);
+            //List<string> fileContentList = System.IO.File.ReadLines(fileName).Where(s => s.Trim() != string.Empty).ToList();
+            List<string> fileContentList = new List<string>();
+
+            if (!String.IsNullOrEmpty(fileName) && (this.HasDirectoryAccess(FileSystemRights.Read, fileName)))
+            {
+                if (File.Exists(fileName))
+                {
+                    try
+                    {
+                        using (TextReader tr = new StreamReader(new FileStream(fileName, FileMode.Open)))
+                        {
+                            string line;
+                            while ((line = tr.ReadLine()) != null)
+                            {
+                                line = line.Trim();
+                                if (line != string.Empty)
+                                {
+                                    fileContentList.Add(line);
+                                }
+
+                            }
+                        }
+
+                        List<FileIndexInfo> fileIndexInfo = new List<FileIndexInfo>();
+
+                        int count = 0;
+                        int fileStartLineIndex = 0;
+                        int fileInvoiceLineIndex = 0;
+                        int fileEndLineIndex = 0;
+                        string invoiceNumber = "";
+                        for (int i = 0; i < fileContentList.Count; i++)
+                        {
+                            string currentLine = fileContentList[i];
+                            if (currentLine == "*")
+                            {
+                                fileStartLineIndex = i;
+                                count++;
+                            }
+                            if (currentLine == "TAX INVOICE")
+                            {
+                                fileInvoiceLineIndex = i + 1;
+                                invoiceNumber = fileContentList[fileInvoiceLineIndex];
+                                count++;
+                            }
+                            if (currentLine == "&")
+                            {
+                                fileEndLineIndex = i;
+                                count++;
+                            }
+                            if (count == 3)
+                            {
+                                FileIndexInfo fi = new FileIndexInfo()
+                                {
+                                    InvoiceNumber = invoiceNumber,
+                                    FileStartLineIndex = fileStartLineIndex,
+                                    FileInvoiceLineIndex = fileInvoiceLineIndex,
+                                    FileEndLineIndex = fileEndLineIndex
+                                };
+
+                                fileIndexInfo.Add(fi);
+                                count = 0;
+                            }
+                        }
+
+                        var result = fileIndexInfo.Where(f => f.InvoiceNumber == inputInvoiceNumber).FirstOrDefault();
+
+                        string directory = Path.GetDirectoryName(fileName);
+                        int startIndex = result.FileStartLineIndex;
+                        int endIndex = result.FileEndLineIndex;
+                        int range = endIndex - startIndex + 1;
+                        string invoiveNum = result.InvoiceNumber;
+                        string newFilePath = directory + "\\" + invoiveNum + ".txt";
+                        List<string> sublist = fileContentList.GetRange(startIndex, range);
+                        //List<string> sublist = fileContentList.Skip(startIndex).Take(endIndex - startIndex);
+
+                        using (TextWriter tw = new StreamWriter(newFilePath))
+                        {
+                            foreach (String s in sublist)
+                                tw.WriteLine(s);
+                        }
+
+                        MessageBox.Show("The subtracted invoice has been saved to: " + newFilePath);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Exception message 1" + ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Exception message 2");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a valid file");
+            }
+        }
+
+        //https://stackoverflow.com/questions/9503884/best-way-to-handle-errors-when-opening-file
+        private bool HasDirectoryAccess(FileSystemRights fileSystemRights, string directoryPath)
+        {
+            DirectorySecurity directorySecurity = Directory.GetAccessControl(directoryPath);
+
+            foreach (FileSystemAccessRule rule in directorySecurity.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount)))
+            {
+                if ((rule.FileSystemRights & fileSystemRights) != 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public class FileIndexInfo
+    {
+        public string InvoiceNumber { get; set; }
+
+        public int FileStartLineIndex { get; set; }
+
+        public int FileInvoiceLineIndex { get; set; }
+
+        public int FileEndLineIndex { get; set; }
     }
 }
